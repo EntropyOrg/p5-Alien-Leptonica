60da710613b65bcb407bc0e4753734f208ceafe2
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-07-12 04:26:09 -0500

    initial commit

---
+++ lib/Alien/Leptonica.pm
@@ -0,0 +1,23 @@
+package Alien::Leptonica;
+
+use strict;
+use warnings;
+
+use parent 'Alien::Base';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Alien::Leptonica - Alien package for the Leptonica image processing library
+
+=head1 COPYRIGHT
+
+Copyright 2013 Zakariyya Mughal.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the Artistic License version 2.0.
+
+=end
