d6e9738a69ad806348e12e86d4525a3d19c5851b
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-07-12 04:26:15 -0500

    Build results of 60da710 (on master)

---
+++ lib/Alien/Leptonica.pm
@@ -0,0 +1,26 @@
+package Alien::Leptonica;
+{
+  $Alien::Leptonica::VERSION = '0.001';
+}
+
+use strict;
+use warnings;
+
+use parent 'Alien::Base';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Alien::Leptonica - Alien package for the Leptonica image processing library
+
+=head1 COPYRIGHT
+
+Copyright 2013 Zakariyya Mughal.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the Artistic License version 2.0.
+
+=end
