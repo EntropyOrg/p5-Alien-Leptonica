acb5d4d376bc68e9a2a30febecce1d1a7339bae4
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-12-24 14:22:43 -0600

    use podweaver

--- lib/Alien/Leptonica.pm
+++ lib/Alien/Leptonica.pm
@@ -8,16 +8,10 @@ use parent 'Alien::Base';
 1;
 
 __END__
+# ABSTRACT: Alien package for the Leptonica image processing library
 
-=head1 NAME
+=pod
 
-Alien::Leptonica - Alien package for the Leptonica image processing library
+=head1 SEE ALSO
 
-=head1 COPYRIGHT
-
-Copyright 2013 Zakariyya Mughal.
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the Artistic License version 2.0.
-
-=end
+L<Leptonica|http://www.leptonica.com/>, L<Image::Leptonica>
