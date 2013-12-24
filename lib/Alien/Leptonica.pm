1fdc7428beca76777b8b2a9eb46c3f7b093cd4eb
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-12-24 14:22:18 -0600

    Build results of 1fb53b4 (on master)

--- lib/Alien/Leptonica.pm
+++ lib/Alien/Leptonica.pm
@@ -10,11 +10,35 @@ use parent 'Alien::Base';
 
 1;
 
-__END__
-# ABSTRACT: Alien package for the Leptonica image processing library
-
 =pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Alien::Leptonica - Alien package for the Leptonica image processing library
+
+=head1 VERSION
+
+version 0.002
 
 =head1 SEE ALSO
 
 L<Leptonica|http://www.leptonica.com/>, L<Image::Leptonica>
+
+=head1 AUTHOR
+
+Zakariyya Mughal <zmughal@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Zakariyya Mughal.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+__END__
+# ABSTRACT: Alien package for the Leptonica image processing library
+
