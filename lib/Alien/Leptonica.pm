94cff2ea4a13a7a0b666098fb382a1d2e91e3b23
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-07-15 19:48:46 -0500

    Release build of v0.005 (on master)

--- lib/Alien/Leptonica.pm
+++ lib/Alien/Leptonica.pm
@@ -1,12 +1,12 @@
 package Alien::Leptonica;
-$Alien::Leptonica::VERSION = '0.006';
+$Alien::Leptonica::VERSION = '0.005';
 use strict;
 use warnings;
 
 use parent 'Alien::Base';
 
 sub Inline {
-	return unless $_[0] eq 'C'; # Inline's error message is good
+	return unless $_[-1] eq 'C'; # Inline's error message is good
 	my $self = __PACKAGE__->new;
 	+{
 		LIBS => $self->libs,
@@ -27,7 +27,7 @@ Alien::Leptonica - Alien package for the Leptonica image processing library
 
 =head1 VERSION
 
-version 0.006
+version 0.005
 
 =head1 Inline support
 
--- t/inline.t
+++ t/inline.t
@@ -1,14 +1,13 @@
 use Test::More;
+use Module::Load;
 
 use_ok('Alien::Leptonica');
 
 SKIP: {
-	eval { require 'Inline' } or do {
+	eval { load 'Inline' } or do {
 		my $error = $@;
 		skip "Inline not installed", 1 if $error;
 	};
-
-	skip "Inline not installed", 1 if $@;
 
 	Inline->import( with => qw(Alien::Leptonica) );
 	Inline->bind( C => q{ extern char * getLeptonicaVersion (  ); },
