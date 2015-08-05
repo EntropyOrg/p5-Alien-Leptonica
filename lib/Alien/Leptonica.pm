027be136f3ecb88aba5b1949f3ccf38dd683dd61
Graham Ollis <plicease@cpan.org>
2015-08-05 15:41:14 -0400

    use the ExtUtils::Depends support that comes with AB

--- lib/Alien/Leptonica.pm
+++ lib/Alien/Leptonica.pm
@@ -3,18 +3,8 @@ package Alien::Leptonica;
 use strict;
 use warnings;
 
-use parent qw(Alien::Base Exporter);
+use parent qw(Alien::Base Exporter );
 our @EXPORT_OK = qw(Inline);
-
-sub Inline {
-	return unless $_[-1] eq 'C'; # Inline's error message is good
-	my $self = __PACKAGE__->new;
-	+{
-		LIBS => $self->libs,
-		INC => $self->cflags,
-		AUTO_INCLUDE => '#include "allheaders.h"',
-	};
-}
 
 1;
 
